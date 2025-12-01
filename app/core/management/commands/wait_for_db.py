2244ee5e9813203ff1350c3a36c92af3bf5f53d8
Oh-SulGi <dhtmfrl0615@naver.com>
2025-12-01 22:10:46 +0900

    setting db connection and core app

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+'''
+django command to wait for the database to be available
+'''
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
