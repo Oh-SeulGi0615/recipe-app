21dedd06ffa1d9da651579702502d7b6bdc10906
Oh-SulGi <dhtmfrl0615@naver.com>
2026-01-14 21:09:37 +0900

    build recipe api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+from core import models
 
 
 class ModelTests(TestCase):
@@ -25,6 +27,7 @@ class ModelTests(TestCase):
 
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
+
             self.assertEqual(user.email, expected)
     
     def test_new_user_without_email_raises_error(self):
@@ -36,5 +39,21 @@ class ModelTests(TestCase):
             'test@example.com',
             'test123'
         )
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user = user,
+            title = 'Sample recipe',
+            time_minutes = 5,
+            price = Decimal('5.50'),
+            description = 'Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
